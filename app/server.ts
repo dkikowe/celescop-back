9de7fb7d33cf6020d0b3819b81767c02cc163823
dkikowe <dkikowe@gmail.com>
2025-10-28 09:31:42 +0500

    cors

--- app/server.ts
+++ app/server.ts
@@ -28,18 +28,18 @@ app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 app.use(cookieParser())
 
 // CORS с поддержкой credentials и preflight
-app.use(
-  cors({
-    origin: [
-      'https://celiscope.ru',
-      'http://localhost:5173'
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  })
-)
-app.options('*', cors())
+// app.use(
+//   cors({
+//     origin: [
+//       'https://celiscope.ru',
+//       'http://localhost:5173'
+//     ],
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+//   })
+// )
+// app.options('*', cors())
 
 
 // Контроллеры
